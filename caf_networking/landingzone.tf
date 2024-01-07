6d33b9fd9407fef842de89973094e5b2c570234a
Hein Tonny Køien <hein.koien@blinq.no>
2024-01-07 12:33:01 +0100

    add folder for caf_networking scenarios to speed up all deployments with networking

---
+++ caf_networking/landingzone.tf
@@ -0,0 +1,28 @@
+module "solution" {
+  # source  = "aztfmod/caf/azurerm"
+  # version = "5.7.7"
+  source = "git::https://github.com/blinqas/terraform-azurerm-caf.git?ref=main"
+
+  providers = {
+    azurerm.vhub = azurerm.vhub
+  }
+
+  resource_groups                       = var.resource_groups
+  networking                            = local.networking
+  azuread                               = local.azuread
+  identity                              = local.identity
+  managed_identities                    = var.managed_identities
+  role_mapping                          = var.role_mapping
+
+  diagnostics = {
+    diagnostic_event_hub_namespaces = try(local.diagnostics.diagnostic_event_hub_namespaces, var.diagnostic_event_hub_namespaces)
+    diagnostic_log_analytics        = try(local.diagnostics.diagnostic_log_analytics, var.diagnostic_log_analytics)
+    diagnostic_storage_accounts     = try(local.diagnostics.diagnostic_storage_accounts, var.diagnostic_storage_accounts)
+    diagnostics_definition          = local.diagnostics.diagnostics_definition
+    diagnostics_destinations        = local.diagnostics.diagnostics_destinations
+    event_hub_namespaces            = local.diagnostics.event_hub_namespaces
+    log_analytics                   = local.diagnostics.log_analytics
+    storage_accounts                = local.diagnostics.storage_accounts
+  }
+
+}
