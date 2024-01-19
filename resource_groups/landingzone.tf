f19387a4e50e5380c3c908c80507837b98798cdf
Hein Tonny Køien <hein.koien@blinq.no>
2024-01-19 20:51:17 +0100

    Test create only resource group with dedicated module

--- resource_groups/landingzone.tf
+++ resource_groups/landingzone.tf
@@ -3,6 +3,10 @@ module "solution" {
   version = "5.7.7"
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
 
+  providers = {
+    azurerm.vhub = azurerm.vhub
+  }
+
   resource_groups                       = var.resource_groups
 
 }
